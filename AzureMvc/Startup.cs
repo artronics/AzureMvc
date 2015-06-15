9d4f8c1571a356ddb0294433085935837ebe541a
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-06-15 08:57:30 +0200

    Initial

---
+++ AzureMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureMvc.Startup))]
+namespace AzureMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
